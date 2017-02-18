1c861d6ddcd663807868832de1000eb60f325dc1
Nightcustard <nightcustard@gmail.com>
2017-02-18 17:16:21 +0000

    Test2.py

--- Test.py
+++ Test.py
@@ -6,15 +6,21 @@ import robo
 
 rs=robo.Robo(21)	#create Robo object for GPIO 21
 rs.send_code(0xB1)	#Issue reset command
-raw_input('Enter')
+# raw_input('Enter')
 rs.send_code(0x81)	#Right arm up
 rs.send_code(0x81)
 rs.send_code(0x82)	#Right wrist out
 rs.send_code(0x85)	#Right wrist in
-rs.send_code(0x82)
-rs.send_code(0x85)
+rs.send_code(0x82)      #
+rs.send_code(0x85)      #
+rs.send_code(0xA0)      #RightTurnStep
+rs.send_code(0xA0)      #RightTurnStep
+rs.send_code(0xC0)      #RHStrike3
+rs.send_code(0xC2)      #Burp
+rs.send_code(0xC3)      #RHStrike3
+rs.send_code(0xC3)
 
-raw_input('Enter')
+# raw_input('Enter')
 rs.send_code(0x89)	#Left arm up
 rs.send_code(0x89)
 for i in range(0,2):
@@ -27,10 +33,11 @@ rs.send_code(0x84)	#Right arm down
 rs.send_code(0x8C)
 rs.send_code(0x84)
 
-raw_input('Enter')
+# raw_input('Enter')
 rs.send_code(0xC4)	#Hi 5
 
-raw_input('Enter')
+# raw_input('Enter')
 rs.send_code(0xCE)	#Roar
+rs.send_code(0xD1)      # Rosebud
 
 print "fin"
