82a70dc5e02bcba2e60ac5d26e4a977949c1965f
Nightcustard <nightcustard@gmail.com>
2017-02-18 17:35:29 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-
+# heavily based upon work by Carl Monk (@ForToffee)
+# based on work from http://playground.arduino.cc/Main/RoboSapienIR
+# command codes originally from http://www.aibohack.com/robosap/ir_codes.htm
