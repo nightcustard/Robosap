fba56fc1274f5d7709d4e7f00821142197e957be
Nightcustard <nightcustard@gmail.com>
2017-02-25 16:16:52 +0000

    Update Shutdown.py

--- Shutdown.py
+++ Shutdown.py
@@ -13,17 +13,17 @@ GPIO.setup(12, GPIO.IN)
 #GPIO 12 is connected to VRE on the Robosap motherboard which changes from 3v3
 #to 0v when 'Rosebud' sleep is executed.
 raw_input("Press Enter when ready\n>")
-print "Waiting for falling edge on GPIO 12"
+#print "Waiting for falling edge on GPIO 12"
 
 try:
     GPIO.wait_for_edge(12, GPIO.FALLING)
-    print "\nFalling edge detected. Going for shutdown"
+    #print "\nFalling edge detected. Going for shutdown"
     #Use falling edge detection to see if pin is pulled 
     #low to avoid repeated polling
     os.system("sudo shutdown -h now")
     #Send command to system to shutdown
 except KeyboardInterrupt:
-    print "Process interrupted, closing"
+    #print "Process interrupted, closing"
     GPIO.cleanup()  #clean up GPIO on CTRL+C exit
 GPIO.cleanup()
 #Revert all GPIO pins to their normal states (i.e. input = safe)
